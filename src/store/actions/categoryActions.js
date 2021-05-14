6c186cebb0418909f57a94f0e46830409f19298d
guille <guille2358@gmail.com>
2021-05-14 16:33:29 -0300

    minor change on addCategory

--- src/store/actions/categoryActions.js
+++ src/store/actions/categoryActions.js
@@ -1,7 +1,7 @@
 export const addCategory = (category) => {
     return (dispatch, getState, { getFirestore }) => {
         const firestore = getFirestore();
-        firestore.collection('category').add({ ...category, favorite: false, createdAt: new Date() }).then(() => {
+        firestore.collection('category').add({ ...category, createdAt: new Date() }).then(() => {
             console.log('Category added successfully')
         }).catch((err) => {
             console.log(err);;
